6219486ead98ce0080855757258741a47a8f984c
BaoNinh2808 <4LegsMonster@gmail.com>
2024-01-23 15:23:24 +0700

    AR firebase

--- app/src/main/java/com/example/mobileappfinal/GUI_layer/Categories/BedFragment.java
+++ app/src/main/java/com/example/mobileappfinal/GUI_layer/Categories/BedFragment.java
@@ -14,8 +14,14 @@ import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mobileappfinal.DTO.Product;
+import com.example.mobileappfinal.Data_layer.Product.ProductDatabase;
 import com.example.mobileappfinal.R;
+
+import java.util.List;
 
 public class BedFragment extends Fragment {
 
@@ -29,6 +35,8 @@ public class BedFragment extends Fragment {
     private String mParam2;
 
     private ImageButton back_button_LvRoom;
+    private RecyclerView recyclerView;
+    private CategoryAdapter categoryAdapter;
     private ImageButton bed1;
     private ImageButton bed2;
     private ImageButton bed3, bed4, bed5, bed6;
@@ -85,78 +93,98 @@ public class BedFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-        bed1 = (ImageButton) v.findViewById(R.id.bed1);
-        bed1.setOnClickListener(new View.OnClickListener() {
+
+         recyclerView = v.findViewById(R.id.recyclerView);
+        ProductDatabase productDatabase = ProductDatabase.getInstance();
+         List<Product> products = productDatabase.getProductByCategory("plant");
+         categoryAdapter = new CategoryAdapter(getContext(), products);
+        categoryAdapter.setOnItemClickListener(new CategoryAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onItemClick(Product product) {
                 if (cnt == 0) {
                     obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed1.obj?alt=media&token=306f9129-061b-40a9-bf2f-0341e8a02e28";
                     png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture3.png?alt=media&token=406a003f-bac4-4e0e-92cb-72bbc791e920";
 //                    cnt = cnt + 1;
                     isObjectReplaced = true;
                 }
+            }
+        });
 
-            }
-        });
-        bed2 = (ImageButton) v.findViewById(R.id.bed2);
-        bed2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cnt == 0) {
-                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed2.obj?alt=media&token=31393109-0840-4019-8cd6-8838e6643ddd";
-                    png_url = "models/table_texture4.png";
-//                    cnt = cnt + 1;
-                    isObjectReplaced = true;
-                }
+        recyclerView.setAdapter(categoryAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 2));
 
-            }
-        });
-        bed3 = (ImageButton) v.findViewById(R.id.bed3);
-        bed3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cnt == 0) {
-                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed3.obj?alt=media&token=4df8f545-5fa7-45c2-bfdc-3410a9dc86c7";
-                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture3.png?alt=media&token=406a003f-bac4-4e0e-92cb-72bbc791e920";
-//                    cnt = cnt + 1;
-                    isObjectReplaced = true;
-                }
-
-            }
-        });
-        bed4 = (ImageButton) v.findViewById(R.id.bed4);
-        bed4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cnt == 0) {
-                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed4.obj?alt=media&token=aac4f370-8b9c-4222-93c1-afdc5b63a4a2";
-                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture5.png?alt=media&token=6219f056-eda6-4286-8ce0-3b8455ac0165";
-                    isObjectReplaced = true;
-                }
-            }
-        });
-        bed5 = (ImageButton) v.findViewById(R.id.bed5);
-        bed5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cnt == 0) {
-                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed1.obj?alt=media&token=306f9129-061b-40a9-bf2f-0341e8a02e28";
-                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture1.png?alt=media&token=5abc06df-f95d-4dbc-a6ea-e6524de703a0";
-                    isObjectReplaced = true;
-                }
-            }
-        });
-        bed6 = (ImageButton) v.findViewById(R.id.bed6);
-        bed6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cnt == 0) {
-                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed2.obj?alt=media&token=31393109-0840-4019-8cd6-8838e6643ddd";
-                    png_url = "models/table_texture2.png";
-                    isObjectReplaced = true;
-                }
-            }
-        });
+//        bed1 = (ImageButton) v.findViewById(R.id.bed1);
+//        bed1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed1.obj?alt=media&token=306f9129-061b-40a9-bf2f-0341e8a02e28";
+//                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture3.png?alt=media&token=406a003f-bac4-4e0e-92cb-72bbc791e920";
+////                    cnt = cnt + 1;
+//                    isObjectReplaced = true;
+//                }
+//
+//            }
+//        });
+//        bed2 = (ImageButton) v.findViewById(R.id.bed2);
+//        bed2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed2.obj?alt=media&token=31393109-0840-4019-8cd6-8838e6643ddd";
+//                    png_url = "models/table_texture4.png";
+////                    cnt = cnt + 1;
+//                    isObjectReplaced = true;
+//                }
+//
+//            }
+//        });
+//        bed3 = (ImageButton) v.findViewById(R.id.bed3);
+//        bed3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed3.obj?alt=media&token=4df8f545-5fa7-45c2-bfdc-3410a9dc86c7";
+//                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture3.png?alt=media&token=406a003f-bac4-4e0e-92cb-72bbc791e920";
+////                    cnt = cnt + 1;
+//                    isObjectReplaced = true;
+//                }
+//
+//            }
+//        });
+//        bed4 = (ImageButton) v.findViewById(R.id.bed4);
+//        bed4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed4.obj?alt=media&token=aac4f370-8b9c-4222-93c1-afdc5b63a4a2";
+//                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture5.png?alt=media&token=6219f056-eda6-4286-8ce0-3b8455ac0165";
+//                    isObjectReplaced = true;
+//                }
+//            }
+//        });
+//        bed5 = (ImageButton) v.findViewById(R.id.bed5);
+//        bed5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed1.obj?alt=media&token=306f9129-061b-40a9-bf2f-0341e8a02e28";
+//                    png_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed_texture1.png?alt=media&token=5abc06df-f95d-4dbc-a6ea-e6524de703a0";
+//                    isObjectReplaced = true;
+//                }
+//            }
+//        });
+//        bed6 = (ImageButton) v.findViewById(R.id.bed6);
+//        bed6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (cnt == 0) {
+//                    obj_url = "https://firebasestorage.googleapis.com/v0/b/mobileappfinal-5d7c6.appspot.com/o/bed2.obj?alt=media&token=31393109-0840-4019-8cd6-8838e6643ddd";
+//                    png_url = "models/table_texture2.png";
+//                    isObjectReplaced = true;
+//                }
+//            }
+//        });
 
 
         return v;
---
+++ app/src/main/java/com/example/mobileappfinal/GUI_layer/Categories/CategoryAdapter.java
@@ -0,0 +1,82 @@
+package com.example.mobileappfinal.GUI_layer.Categories;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobileappfinal.DTO.Product;
+import com.example.mobileappfinal.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private List<Product> productList;
+    private Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public CategoryAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+        this.onItemClickListener = null;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.category_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Product product);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Product product = productList.get(position);
+
+        holder.categoryName.setText(product.getName());
+        Picasso.get().load(product.getImageUrl()).into(holder.categoryImage);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pass the clicked item to the external click handler
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(product);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView categoryName;
+        ImageView categoryImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.categoryCardView);
+            categoryName = itemView.findViewById(R.id.categoryName);
+            categoryImage = itemView.findViewById(R.id.categoryImage);
+        }
+    }
+}
+
